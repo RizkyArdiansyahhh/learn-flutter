2de96ee6956c9ad13ea78d284715d0e92b888041
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-01-22 09:08:12 +0700

    day 2 -> box decoration

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -7,37 +7,44 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Container"),
+          title: const Text("Box Decoration"),
         ),
-        body: Column(children: [
-          Row(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                width: 100,
-                height: 100,
-                color: Colors.blue,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.red,
-              )
+                  margin: EdgeInsets.only(bottom: 10),
+                  height: 200,
+                  width: 200,
+                  // color: Colors.red,
+                  decoration: BoxDecoration(
+                    // color: Colors.red,
+                    // borderRadius: BorderRadius.only(
+                    //     bottomLeft: Radius.circular(100),
+                    //     topRight: Radius.circular(100)),
+                    // gradient: LinearGradient(
+                    //     colors: [Colors.yellow, Colors.green],
+                    //     begin: Alignment.topLeft,
+                    //     end: Alignment.bottomRight),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 15,
+                        spreadRadius: 2,
+                        color: Colors.black54,
+                        offset: Offset(1, 1),
+                      )
+                    ],
+                    border: Border.all(color: Colors.black, width: 2),
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://i.pinimg.com/originals/0a/8a/59/0a8a5939bfd3bec705b3a4c52a07cb1a.jpg"),
+                        fit: BoxFit.cover),
+                    shape: BoxShape.circle,
+                  )),
+              const Text("User123"),
             ],
           ),
-          Row(
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.green,
-              )
-            ],
-          )
-        ]));
+        ));
   }
 }
