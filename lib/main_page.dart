791f43a8543d75fc4891e4872ed6689929940593
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-01-22 09:51:00 +0700

    day 2 -> SizedBox

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -7,44 +7,19 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Box Decoration"),
+          title: Text("SizedBox Widget"),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                  margin: EdgeInsets.only(bottom: 10),
-                  height: 200,
-                  width: 200,
-                  // color: Colors.red,
-                  decoration: BoxDecoration(
-                    // color: Colors.red,
-                    // borderRadius: BorderRadius.only(
-                    //     bottomLeft: Radius.circular(100),
-                    //     topRight: Radius.circular(100)),
-                    // gradient: LinearGradient(
-                    //     colors: [Colors.yellow, Colors.green],
-                    //     begin: Alignment.topLeft,
-                    //     end: Alignment.bottomRight),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        spreadRadius: 2,
-                        color: Colors.black54,
-                        offset: Offset(1, 1),
-                      )
-                    ],
-                    border: Border.all(color: Colors.black, width: 2),
-                    image: DecorationImage(
-                        image: NetworkImage(
-                            "https://i.pinimg.com/originals/0a/8a/59/0a8a5939bfd3bec705b3a4c52a07cb1a.jpg"),
-                        fit: BoxFit.cover),
-                    shape: BoxShape.circle,
-                  )),
-              const Text("User123"),
-            ],
-          ),
+          child: Column(children: [
+            SizedBox(
+              width: 200,
+              child: Text("Halo Nama Saya Rizky Ardiansyah Dari Pekanbaru"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text("Saya Berumur 20 Tahun"),
+          ]),
         ));
   }
 }
