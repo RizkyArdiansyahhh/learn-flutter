9437f655a6d8b845bf07923a6b7d178d0b727090
RizkyArdiansyahhh <rizky.ardiansyahhh06@gmail.com>
2025-01-28 22:20:34 +0700

    day 4 -> fieldText decoration

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -9,43 +8,69 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  TextEditingController textEditingController =
-      TextEditingController(text: "initial value");
+  TextEditingController textEditingController = TextEditingController(text: "");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("TextField Widget")),
-          backgroundColor: Colors.greenAccent,
+      appBar: AppBar(
+        title: Center(
+          child: Text("TextField Widget"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              TextField(
-                controller: textEditingController,
-                // obscureText: true,
-                // obscuringCharacter: "!",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
+        backgroundColor: Colors.grey.shade400,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 25,
+            ),
+            TextField(
+              controller: textEditingController,
+              onChanged: (value) => setState(() {}),
+              style: TextStyle(
+                fontSize: 16,
+              ),
+              // autofocus: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                // PlaceHolder input
+                hintText: "Enter Your Name",
+                hintStyle: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey.shade500,
                 ),
-                cursorColor: Colors.red,
-                // inputFormatters: [
-                //   // Menggunakan TextInputFormatter untuk memaksa input menjadi kapital
-                //   FilteringTextInputFormatter.allow(RegExp('[A-Za-z]')),
-                //   TextInputFormatter.withFunction((oldValue, newValue) =>
-                //       TextEditingValue(text: newValue.text.toUpperCase())),
-                // ],
-                onChanged: (value) => setState(() {}),
+
+                // Label Input
+                labelText: "Username",
+                labelStyle: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+
+                // Counter Input
+                counterText: textEditingController.text.length >= 50
+                    ? "50"
+                    : "${50 - textEditingController.text.length}",
+                counterStyle: TextStyle(
+                  color: textEditingController.text.length >= 40
+                      ? Colors.red
+                      : Colors.black,
+                ),
+
+                // Error Input
+                errorText: textEditingController.text.length >= 50
+                    ? "Max Username 50"
+                    : null,
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(textEditingController.text,
-                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16)),
-            ],
-          ),
-        ));
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(textEditingController.text),
+          ],
+        ),
+      ),
+    );
   }
 }
